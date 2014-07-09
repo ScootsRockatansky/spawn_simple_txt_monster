9be38b6627e984e9d98988daf7cf139378035532
ScootsRockatansky <maxdolphin2008@gmail.com>
2014-07-09 15:56:07 -0400

    Update weapons_test.py
    The finished product

--- weapons_test.py
+++ weapons_test.py
@@ -1,22 +1,36 @@
+
 from random import random, randint
+#1out of 5 chance for critical hit
+#start with class weapons
+#child class sword, less damage, more chance of crit
+#child class mace, more damage, less crit
+#crit=base*2
+strength=75
 
-monster_hp = 1000
-
-import random
-
-class Weapon:
-    dmg = 150
-    crithit = randint(0,4)
-    if crithit == 2:
-        dmg = dmg * 2
-    else:
-         = dmg
-
-cmd = raw_input("Type attack!")
-
-if cmd == 'attack':
-    monster_hp = monster_hp - 
-    
-
-    print(monster_hp)
-
+class sword:
+    def swordhit(self):
+        attacks = randint(strength/2, strength*2)
+        chance= randint(1,6)
+        if chance== 3:
+            attacks= randint(strength, strength*3)
+            print 'CRITICAL HIT!'
+        return attacks
+        
+class mace:
+    def macehit(self):
+        attackm= randint(strength/2, strength*2)
+        chance= randint(1,15)
+        if chance==3:
+            attackm= randint(strength, strength*4)
+            print "CRITICAL HIT!"
+        return attackm
+        
+monster_hp=200
+sw = sword()
+mc=mace()
+cmd= raw_input()
+if cmd=="attack":
+    monster_hp = monster_hp - sw.swordhit()
+    print monster_hp
+else:
+    print "no"
